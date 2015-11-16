3a850cbfc489b58044c2a206dc61e0b7eb3c96a0
Dan Stocker <dan.stocker@mangahigh.com>
2015-11-16 14:56:42 +0000

    removes Q dependency from remaining modules

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function (config) {
             'node_modules/giant-assertion/lib/giant-assertion.js',
             'node_modules/giant-oop/lib/giant-oop.js',
             'config/giant-oop-test.config.js',
-            'node_modules/q/q.js',
             'node_modules/giant-utils/lib/giant-utils.js',
             'node_modules/giant-data/lib/giant-data.js',
             'node_modules/giant-event/lib/giant-event.js',
